0faca015bd90f395c0584f4b69bf6752b21694f7
Zechary Chua <zechary28@gmail.com>
2026-07-25 18:42:04 +0800

    Enable fullscreen and monitor switching

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@
 #include <vector>
 #include <memory>
 
-constexpr int PARTIAL_SCREEN_WIDTH = 1200;
-constexpr int PARTIAL_SCREEN_HEIGHT = 800;
+constexpr int WINDOW_WIDTH = 1200;
+constexpr int WINDOW_HEIGHT = 800;
 
 constexpr int CELL_SIZE = 20;
 
@@ -22,20 +22,15 @@ struct GameSlot {
     std::function<std::unique_ptr<IGame>(ThemeColor)> make;
 };
 
-int main() 
-{            
-    InitWindow(100, 100, "Minesweeper");  // temporary small window to init raylib
+int main()
+{   
+    SetConfigFlags(FLAG_WINDOW_UNDECORATED | FLAG_WINDOW_ALWAYS_RUN);
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Minesweeper");
 
-    int currentMonitor = 0;
-    int screenWidth  = PARTIAL_SCREEN_WIDTH;
-    int screenHeight = PARTIAL_SCREEN_HEIGHT;
-
-    // round down window dimensions
-    screenWidth  = (screenWidth  / CELL_SIZE) * CELL_SIZE;
-    screenHeight = (screenHeight / CELL_SIZE) * CELL_SIZE;
-    SetWindowSize(screenWidth, screenHeight);
-    //ToggleBorderlessWindowed();
-    //ToggleFullscreen();
+    bool isFullscreen = false;
+    int screenWidth  = WINDOW_WIDTH;
+    int screenHeight = WINDOW_HEIGHT;
+    int monitor = 0;
 
     int gridW = screenWidth / CELL_SIZE;
     int gridH = screenHeight / CELL_SIZE;
@@ -44,7 +39,7 @@ int main()
     
     std::vector<GameSlot> slots = {
         {
-            [=](ThemeColor t) -> std::unique_ptr<IGame> {
+            [&](ThemeColor t) -> std::unique_ptr<IGame> {
                 return std::make_unique<Minesweeper>(gridW, gridH, CELL_SIZE, 0.13f, t);
             }
         }
@@ -73,69 +68,48 @@ int main()
     };
 
     std::unique_ptr<IGame> current = slots[gameIndex].make(makeTheme());
+
+    SetWindowState(FLAG_WINDOW_ALWAYS_RUN);
  
     while (!WindowShouldClose())
     {        
 
-        if (IsKeyPressed(KEY_M))  // cycle through monitors
+        if (IsKeyPressed(KEY_F))  // cycle through monitors
         {
-            // int monitorCount = GetMonitorCount();
-            // int nextMonitor = (GetCurrentMonitor() + 1) % monitorCount;
-            // std::cout << "monitorCount:   " + std::to_string(monitorCount) + "\n";
-            // std::cout << "currentMonitor: " + std::to_string(GetCurrentMonitor()) + "\n";
-            // std::cout << "nextMonitor:    " + std::to_string(nextMonitor) + "\n";
+            if (!isFullscreen)
+            {
+                // go fullscreen on whichever monitor the window is currently on
+                screenWidth  = GetMonitorWidth(monitor);
+                screenHeight = GetMonitorHeight(monitor);
+                SetWindowSize(screenWidth, screenHeight);
+                ToggleFullscreen();
+                isFullscreen = true;
+            }
+            else
+            {
+                // back to fixed window
+                ToggleFullscreen();
+                SetWindowState(FLAG_WINDOW_UNDECORATED);
+                screenWidth  = WINDOW_WIDTH;
+                screenHeight = WINDOW_HEIGHT;
+                SetWindowSize(screenWidth, screenHeight);
+                isFullscreen = false;
+            }
 
-            // std::cout << "currentMonitorPos: " + std::to_string(GetMonitorPosition(currentMonitor).x) + ", " + std::to_string(GetMonitorPosition(currentMonitor).y) + "\n";
-            // std::cout << "nextMonitorPos:    " + std::to_string(GetMonitorPosition(nextMonitor).x) + ", " + std::to_string(GetMonitorPosition(nextMonitor).y) + "\n";
-
-            // std::cout << "currentMonitorDim: " + std::to_string(GetMonitorWidth(currentMonitor)) + ", " + std::to_string(GetMonitorHeight(currentMonitor)) + "\n";
-            // std::cout << "nextMonitorDim:    " + std::to_string(GetMonitorWidth(nextMonitor)) + ", " + std::to_string(GetMonitorHeight(nextMonitor)) + "\n";
-
-            // // get target monitor's position and dimensions
-            // Vector2 pos = GetMonitorPosition(nextMonitor);
-            // int w = GetMonitorWidth(nextMonitor);
-            // int h = GetMonitorHeight(nextMonitor);
-
-            // ToggleBorderlessWindowed();   // back to normal window
-            // SetWindowPosition((int)pos.x, (int)pos.y);
-            // SetWindowSize(w, h);
-            // ToggleBorderlessWindowed();   // borderless on new monitor
-
-            // currentMonitor = nextMonitor;
-            // screenWidth  = w;
-            // screenHeight = h;
-            // gridW = screenWidth / CELL_SIZE;
-            // gridH = screenHeight / CELL_SIZE;
-
-            // gameIndex = 0;
-            // current = slots[gameIndex].make(makeTheme());
-
-            ToggleFullscreen();
-
-            int w = PARTIAL_SCREEN_WIDTH;
-            int h = PARTIAL_SCREEN_HEIGHT;
-            screenWidth  = w;
-            screenHeight = h;
+            // recreate game for new dimensions
             gridW = screenWidth / CELL_SIZE;
             gridH = screenHeight / CELL_SIZE;
-
-            gameIndex = 0;
-            current = slots[gameIndex].make(makeTheme());
+            gameIndex = (gameIndex + 1) % static_cast<int>(slots.size());
+            current = nextGame();
 
         }
 
-        // check for new window dimensions, update grid accordingly
-        int newW = GetMonitorWidth(currentMonitor);
-        int newH = GetMonitorHeight(currentMonitor);
-        if (newW != screenWidth || newH != screenHeight)
+        // check if monitor has changed, DO NOTHING FOR NOW
+        int currentMonitor = GetCurrentMonitor();
+        if (currentMonitor != monitor)
         {
-            screenWidth  = newW;
-            screenHeight = newH;
-            gridW = screenWidth / CELL_SIZE;
-            gridH = screenHeight / CELL_SIZE;
-
-            gameIndex = 0;
-            current = slots[i].make(makeTheme());
+            monitor = currentMonitor;
+            std::cout << "changing monitor to " << std::to_string(monitor) << "\n";
         }
 
         if (current->IsOver()) 
